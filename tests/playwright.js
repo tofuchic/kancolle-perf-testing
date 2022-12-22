4ed24cf80ca099afe6c71c9ba8eaba20a8b68767
tofuchic <tofuchic@protonmail.com>
2022-12-23 01:08:50 +0900

    codegenしたスクリプトをそのまま追加

---
+++ tests/playwright.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://tofuchic.github.io/kancolle/');
+  await page.getByRole('button', { name: 'open drawer' }).click();
+  await page.getByRole('link', { name: 'かんみのみかん' }).click();
+  await page.locator('.MuiBackdrop-root').click();
+  await page.getByRole('link', { name: '2022年11月号' }).click();
+  await page.getByRole('link', { name: 'ログインしてみかんをレビューしよう' }).click();
+  await page.getByRole('button', { name: 'Twitterでログイン' }).click();
+  await page.getByRole('heading', { name: 'Authorize mikancolle to access your account?' }).click();
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByLabel('Phone, email, or username').click();
+  await page.getByLabel('Phone, email, or username').fill('yourTwitterAccountName');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('textbox', { name: 'Password Reveal password' }).fill('yourTwitterAccountPassword');
+  await page.getByTestId('LoginForm_Login_Button').click();
+  await page.getByRole('button', { name: 'ログアウト' }).click();
+});
